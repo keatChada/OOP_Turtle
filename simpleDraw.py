03115a268639a1a8623813fd3885f20dd8465011
keatChada <69413088+keatChada@users.noreply.github.com>
2022-05-14 10:22:31 +0700

    Turtle classes for drawing

---
+++ simpleDraw.py
@@ -0,0 +1,28 @@
+#https://docs.python.org/2/library/turtle.html#turtle.shape
+import turtle
+
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    brad = turtle.Turtle()
+    brad.shape("turtle") #2-step
+    brad.color("yellow") #2-step
+    brad.speed(2)        #2-step
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+
+    angie = turtle.Turtle() #3-step
+    angie.shape("arrow") #3-step
+    angie.color("blue") #3-step
+    angie.circle(100) #3-step
+
+    window.exitonclick()
+
+draw_art()
